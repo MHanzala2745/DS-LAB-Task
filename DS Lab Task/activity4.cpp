7b72221ac92fabb1a99cdc1465a05e1c82fd11b1
MHanzala2745 <113432967+MHanzala2745@users.noreply.github.com>
2022-09-13 15:30:00 +0500

    Add files via upload

---
+++ DS Lab Task/activity4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string mystring;
+	mystring = "this is the initial string content";
+	cout << mystring << endl;
+	mystring = "this is different string content";
+	cout << mystring << endl;
+	return 0;
+}
